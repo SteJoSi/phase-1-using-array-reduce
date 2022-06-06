b6a88f292fe29cbcc01fb22906fa38e481231070
Stephanie Sison <sjsison27@icloud.com>
2022-06-06 16:55:35 -0700

    commit tree 4e82cdf0f95a3a456d99b6310f57593b0c10e360

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//function totalBatteries(batteryBatches, reducer, init) {
+   // let accum = init;
+   // for (const element of batteryBatches) {
+//         accum = reducer(accum, element);
+//     }
+//     return accum;
+// }
+
+const totalBatteries = batteryBatches.reduce((a, b)=> a + b)
